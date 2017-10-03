a78fcac508e1de5845da062d24c8ec780043d670
Covina <jabs303@gmail.com>
2017-10-03 06:14:43 -0700

    Section 4, Lecture 30: Setting up menu animations, basketball selection;

---
+++ Assets/Scripts/Main Menu Controller/MainMenuController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+
+    // Animators
+    private Animator menuButtonAnimator;
+    private Animator ballSelectAnimator;
+
+
+    private void Awake()
+    {
+        // main menu buttons    
+        menuButtonAnimator = GameObject.Find("Menu Holder").GetComponent<Animator>();
+
+        // Get the basketball color selector
+        ballSelectAnimator = GameObject.Find("Balls Holder").GetComponent<Animator>();
+
+    }
+
+    public void PlayGame()
+    {
+        // load the game scene
+        SceneManager.LoadScene("Gameplay");
+    }
+
+
+    // player wants to select ball color
+    public void LoadBallSelection()
+    {
+        // fade out the menu buttons
+        menuButtonAnimator.Play("FadeOut");
+
+        // bring in the ball selection
+        ballSelectAnimator.Play("FadeIn");
+
+    }
+
+    // Player wants to go back to the main menu
+    public void BackToMenu()
+    {
+
+        // fade out the ball selection
+        ballSelectAnimator.Play("FadeOut");
+
+        // fade in the menu buttons
+        menuButtonAnimator.Play("FadeIn");
+
+
+    }
+
+}
---
+++ Assets/Scripts/Main Menu Controller/SelectBall.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectBall : MonoBehaviour {
+
+    // Create list to hold all of our buttons
+    private List<Button> ballButtons = new List<Button>();
+
+
+    private void Awake()
+    {
+        // Add the onclick behavior for selecting balls
+        GetButtonsAndAddListeners();
+    }
+
+    // Find all the ball button options and add select functionality
+    void GetButtonsAndAddListeners()
+    {
+        // Find all available ball buttons
+        GameObject[] btns = GameObject.FindGameObjectsWithTag("MenuBall");
+
+        // Loop through the found buttons
+        for(int i = 0; i < btns.Length; i++)
+        {
+            // add the button components to the list
+            ballButtons.Add( btns[i].GetComponent<Button>() );
+
+            // add an OnClick listener function to each button we just added to the list
+            ballButtons[i].onClick.AddListener( () => SelectABall() );
+
+        }
+
+    }
+
+    // Equip the ball 
+    public void SelectABall()
+    {
+        // Get the ball int from its game object name when it's selected by the player
+        int ballIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+
+        //Debug.Log("The selected ball is [" + ballIndex + "]");
+
+        // Inform the GameManager which ball the player equipped
+
+    }
+
+}
